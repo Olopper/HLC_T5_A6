590fd7c39ff079150c66dafe622f2a429f83391f
Olopper <olopper2407@g.educaand.es>
2025-02-20 08:59:13 +0100

    Exercise 3

---
+++ Ejercicio 2.py
@@ -0,0 +1,10 @@
+def clave(key):
+    key = 0
+    import random
+    for clave in key:
+     numero = random.randint(1, 10)
+     caracter = random.randint #, @, &, €
+     letra = random.randint("a","b","c","d","e","f","g","h","i","j","k","l")
+
+
+print(clave())
